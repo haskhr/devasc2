f6ca2f6ac576b67d5d454863e326596b29973844
nick <njrusmc@Gmail.com>
2019-08-11 13:02:16 -0400

    pytest unit test cleanup

--- m5/src/test_unit.py
+++ m5/src/test_unit.py
@@ -26,6 +26,7 @@ def test_balance(db_mock):
     assert db_mock.balance("ACCT100") == "40.00 USD"
     assert db_mock.balance("ACCT200") == "-10.00 USD"
     assert db_mock.balance("ACCT300") == "0.00 USD"
+    assert db_mock.balance("nick123") is None
 
 
 def test_owes_money(db_mock):
@@ -36,4 +37,4 @@ def test_owes_money(db_mock):
     assert db_mock.owes_money("ACCT100")
     assert not db_mock.owes_money("ACCT200")
     assert not db_mock.owes_money("ACCT300")
-    assert not db_mock.owes_money("nick123")
+    assert db_mock.owes_money("nick123") is None
